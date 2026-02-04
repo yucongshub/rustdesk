d5ae8f0fe8a62f3d1a5148936114b486ea6e2074
yuc <yuc@yucdeMacBook-Air.local>
2026-02-04 15:13:29 +0800

    bypass secure_tcp

--- src/client.rs
+++ src/client.rs
@@ -421,12 +421,7 @@ impl Client {
             NatType::from_i32(my_nat_type).unwrap_or(NatType::UNKNOWN_NAT)
         };
 
-        if !key.is_empty() && !token.is_empty() {
-            // mainly for the security of token
-            secure_tcp(&mut socket, &key)
-                .await
-                .map_err(|e| anyhow!("Failed to secure tcp: {}", e))?;
-        } else if let Some(udp) = udp.1.as_ref() {
+        if let Some(udp) = udp.1.as_ref() {
             let tm = Instant::now();
             loop {
                 let port = *udp.lock().unwrap();
