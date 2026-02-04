39ca60b46bb5ff0372b351f3a406fd92b9f0e477
yuc <yuc@yucdeMacBook-Air.local>
2026-02-04 15:44:01 +0800

    bypass secure_tcp when request relay

--- src/client.rs
+++ src/client.rs
@@ -845,11 +845,6 @@ impl Client {
                 .await
                 .with_context(|| "Failed to connect to rendezvous server")?;
 
-            if !key.is_empty() && !token.is_empty() {
-                // mainly for the security of token
-                secure_tcp(&mut socket, key).await?;
-            }
-
             ipv4 = socket.local_addr().is_ipv4();
             let mut msg_out = RendezvousMessage::new();
             uuid = Uuid::new_v4().to_string();
